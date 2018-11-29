37bf6536667867ac84f8a669372cbcc8b2275e9d
piyush kumar sahu <piyushkumarsahu51@gmail.com>
2018-11-30 02:34:03 +0530

    first template

--- Customer Billling Information System/customerbilling/settings.py
+++ Customer Billling Information System/customerbilling/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'customer.apps.CustomerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'firstapp.urls'
+ROOT_URLCONF = 'customerbilling.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'firstapp.wsgi.application'
+WSGI_APPLICATION = 'customerbilling.wsgi.application'
 
 
 # Database
